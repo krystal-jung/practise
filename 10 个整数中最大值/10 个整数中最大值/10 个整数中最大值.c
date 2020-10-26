0c199ad4c04239854520fbc4f10380d32d7b8930
hiddenblue <273570606@qq.com>
2020-10-26 11:08:34 +0800

    正确值

---
+++ 10 个整数中最大值/10 个整数中最大值/10 个整数中最大值.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int arr[] = { 5,1200,15,16,10,99,76,44,15,85 };
+	int max = arr[0];
+	int i;
+	for ( i = 1; i <= 9; i++){
+		int temp;
+		if (max < arr[i]){	
+			max = arr[i];			
+		}
+	}
+	if (max >= arr[i]){
+		printf("%d\n", max);
+	}
+	system("pause");
+	return 0;
+
+}
