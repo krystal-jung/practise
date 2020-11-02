024b99565dca4ee93b2b0c895108bd1dbde3d2a3
hiddenblue <273570606@qq.com>
2020-11-02 10:55:35 +0800

    宏定义数组内元素个数NUMBER方便修改

--- 交换数组/交换数组/交换数组.c
+++ 交换数组/交换数组/交换数组.c
@@ -1,15 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
-
+#define NUMER 5
 #include<stdio.h>
 int main()
 {
-	int arr1[5] = {0};
-	int arr2[5] = {0};
-	printf("请输入数组arr1的元素（5个）：");
+	int arr1[NUMER] = { 0 };
+	int arr2[NUMER] = { 0 };
+	printf("请输入数组arr1的元素（NUMER）：");
 	for (int i = 0; i < 5; i++){
 		scanf("%d",&arr1[i]);
 	}
-	printf("请输入数组arr2的元素（5个）：");
+	printf("请输入数组arr2的元素（NUMER）：");
 	for (int i = 0; i < 5; i++){
 		scanf("%d",&arr2[i]);
 	}
@@ -18,12 +18,12 @@ int main()
 		arr1[i] = arr2[i];
 		arr2[i] = temp;
 	}
-	printf("输出数组arr1的元素（5个）：");
+	printf("输出数组arr1的元素（NUMER）：");
 	for (int i = 0; i < 5; i++){
 		printf("%d ", arr1[i]);
 	}
 	printf("\n");
-	printf("输出数组arr1的元素（5个）：");
+	printf("输出数组arr1的元素（NUMER）：");
 	for (int  i = 0; i< 5; i++){
 		printf("%d ", arr2[i]);
 	}
