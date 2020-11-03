cfccd5f7372dd4d1bc7b05b9990eb4998e334b05
hiddenblue <273570606@qq.com>
2020-11-03 13:15:47 +0800

    方法二

--- 整数二进制的1的个数/整数二进制的1的个数/整数二进制的1的个数.c
+++ 整数二进制的1的个数/整数二进制的1的个数/整数二进制的1的个数.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	int num = 10;
+	/*int num = 10;
 	int count = 0;
 	while (num){
 		if (num % 2 == 1){
@@ -10,7 +10,16 @@ int main()
 		}
 		num = num / 2;
 	}
-	printf("%d",count);
+	printf("%d",count);*/
+	int num = -1; 
+	int i = 0;
+	int count = 0;
+	for (i = 0; i < 32; i++){
+		if (((num >> i) & 1) == 1){
+			count++;
+		}
+	}
+	printf("%d", count);
 	system("pause");
 	return 0;
 }
