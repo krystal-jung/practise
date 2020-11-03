115bdb67d11077c224ded625427ef16af001edd1
hiddenblue <273570606@qq.com>
2020-11-03 13:17:48 +0800

    方法三

--- 整数二进制的1的个数/整数二进制的1的个数/整数二进制的1的个数.c
+++ 整数二进制的1的个数/整数二进制的1的个数/整数二进制的1的个数.c
@@ -11,13 +11,22 @@ int main()
 		num = num / 2;
 	}
 	printf("%d",count);*/
-	int num = -1; 
+	/*int num = -1; 
 	int i = 0;
 	int count = 0;
 	for (i = 0; i < 32; i++){
 		if (((num >> i) & 1) == 1){
 			count++;
 		}
+	}
+	printf("%d", count);*/
+	int num = -1;
+	int i = 0;
+	int count = 0;
+	while (num){
+
+		count++;
+		num = num&(num - 1);
 	}
 	printf("%d", count);
 	system("pause");
