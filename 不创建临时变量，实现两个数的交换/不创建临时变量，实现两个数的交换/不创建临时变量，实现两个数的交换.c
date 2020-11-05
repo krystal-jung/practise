b4f66376f33ef522734a62a7d1439e57ce758a56
hiddenblue <273570606@qq.com>
2020-11-05 14:25:30 +0800

    1105

--- 不创建临时变量，实现两个数的交换/不创建临时变量，实现两个数的交换/不创建临时变量，实现两个数的交换.c
+++ 不创建临时变量，实现两个数的交换/不创建临时变量，实现两个数的交换/不创建临时变量，实现两个数的交换.c
@@ -2,12 +2,13 @@
 #include <windows.h>
 int main()
 {
-	int a = 10;
-	int b = 20;
+	int a = 15;
+	int b = 25;
 	a = a^b;
 	b = a^b;
 	a = a^b;
 	printf("a=%d b=%d\n", a, b);
+	Sleep(1000);
 	system("pasue");
 	return 0;
 }
