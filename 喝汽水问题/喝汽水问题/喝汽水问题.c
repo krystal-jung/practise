cbe7b7605cef76e1694c64ecd2845b2b7d9414b1
hiddenblue <273570606@qq.com>
2020-11-08 18:27:52 +0800

    喝汽水

---
+++ 喝汽水问题/喝汽水问题/喝汽水问题.c
@@ -0,0 +1,22 @@
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水
+#include <stdio.h>
+int main()
+{
+	int money = 20;
+	int price = 1;
+	int count = 0;
+	if (money > 2){
+		count = money / price;
+		int empty = count;
+		while (empty > 1){
+			count = count + empty / 2;
+			empty = empty / 2 + empty % 2;
+		}
+	}
+	else{
+		count = money / price;
+	}
+	printf("%d\n", count);
+	system("pause");
+	return 0;
+}
