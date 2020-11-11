e52788d39469bedc829f2a2ba82f95f1e9cef1e8
hiddenblue <273570606@qq.com>
2020-11-11 12:41:24 +0800

    猜凶手

--- 猜凶手/猜凶手/猜凶手.c
+++ 猜凶手/猜凶手/猜凶手.c
@@ -9,9 +9,8 @@
 #include <stdio.h>
 int main()
 {  
-	int murder = 0;
-	for (murder = 'A'; murder <= 'D'; murder++){
-		if ((murder != 'A' + murder == 'C' + murder == 'D' + murder != 'D') == 3){
+	for (int murder = 'A'; murder <= 'D'; murder++){
+		if (((murder != 'A') + (murder == 'C' )+( murder == 'D') +( murder != 'D')) == 3){
 			printf("Ğ×ÊÖÊÇ%c", murder);
 		}
 	}
