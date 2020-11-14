7fd1d38db6326e8126d37f7e717237e99537ebd0
hiddenblue <273570606@qq.com>
2020-11-14 20:47:42 +0800

    回调函数冒泡排序

---
+++ 回调函数冒泡排序/回调函数冒泡排序/回调函数冒泡排序.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+typedef int(*Cmp)(int a, int b);
+void bubbleSort(int arr[], int size, Cmp cmp){
+	for (int bound = 0; bound < size; bound++) {
+		for (int cur = size - 1; cur > bound; cur--) {
+			if (cmp(arr[cur - 1], arr[cur]) == 1) {
+				int tmp = arr[cur - 1];
+				arr[cur - 1] = arr[cur];
+				arr[cur] = tmp;
+			}
+		}
+	}
+}
+int less(int a, int b){
+	if (a == b){
+		return 0;
+	}
+	if (a < b) {
+		return -1;
+	}
+	return 1;
+}
+int main()
+{
+	int arr[] = { 9, 5, 2, 7 };
+	bubbleSort(arr, 4, less);
+	for (int i = 0; i < 4; i++) {
+		printf("%d\n", arr[i]);
+	}
+	system("pause");
+	return 0;
+}
