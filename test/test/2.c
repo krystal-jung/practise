6e977ce19b800eb5a086319b22e31a365bb931af
hiddenblue <273570606@qq.com>
2020-11-17 14:14:59 +0800

    二进制序列

--- test/test/2.c
+++ test/test/2.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 
 
-typedef int(*Cmp)(int a, int b);
+//typedef int(*Cmp)(int a, int b);
 
-void bubbleSort(int arr[], int size, Cmp cmp){
-	for (int bound = 0; bound < size; bound++) {
-		for (int cur = size - 1; cur > bound; cur--) {
-			if (cmp(arr[cur - 1], arr[cur]) == 1) {
-				int tmp = arr[cur - 1];
-				arr[cur - 1] = arr[cur];
-				arr[cur] = tmp;
-			}
-		}
-	}
-}
-int less(int a, int b){
-	if (a == b){
-		return 0;
-	}
-	if (a < b) {
-		return -1;
-	}
-	return 1;
-}
+//void bubbleSort(int arr[], int size, Cmp cmp){
+//	for (int bound = 0; bound < size; bound++) {
+//		for (int cur = size - 1; cur > bound; cur--) {
+//			if (cmp(arr[cur - 1], arr[cur]) == 1) {
+//				int tmp = arr[cur - 1];
+//				arr[cur - 1] = arr[cur];
+//				arr[cur] = tmp;
+//			}
+//		}
+//	}
+//}
+//int less(int a, int b){
+//	if (a == b){
+//		return 0;
+//	}
+//	if (a < b) {
+//		return -1;
+//	}
+//	return 1;
+//}
 int main()
-{
+{ /*
 	int arr[] = { 9, 5, 2, 7 };
 	bubbleSort(arr, 4, less);
 	for (int i = 0; i < 4; i++) {
 		printf("%d\n", arr[i]);
+	}*/
+ 
+	int num = 11;
+	for (int i = 30; i >= 0; i -= 2){
+		printf("%d ", ((num >> i) & 1));
 	}
+	for (int i = 31; i >= 0; i -= 2){
+		printf("%d ", ((num >> i) & 1));
+	}
+	
 	system("pause");
 	return 0;
 }
