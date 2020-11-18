5075d7d90090f372eba48d50b5a7254d0fda0f64
hiddenblue <273570606@qq.com>
2020-11-18 19:56:53 +0800

    模拟实现strcmp

--- 模拟实现strcmp/模拟实现strcmp/模拟实现strcmp.c
+++ 模拟实现strcmp/模拟实现strcmp/模拟实现strcmp.c
@@ -15,9 +15,16 @@ int myStrcmp(const char* str1, const char* str2){
 			str2++;
 		}	
 	}
-	return *str1 - *str2;
+	if (*str1 < *str2) {
+		return -1;
+	}
+	else if (*str1 > *str2) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
-
 int main()
 {
 	char str1[] = "hehehe";
